18e3492d126d05e57a8267092e0c8e86d1848f9a
zhanchi5 <zhanchi5@users.noreply.github.com>
2017-10-14 19:10:17 +0300

    Create init.cpp

---
+++ tests/source/init.cpp
@@ -0,0 +1,21 @@
+#include <catch.hpp>
+
+#include "stack0.0.1.hpp"
+
+TEST_CASE("push") 
+{
+    stack<int> stack_;
+    stack_.push(32);
+    stack_.push(64);
+    REQUIRE(stack_.count() == 2);
+}
+
+TEST_CASE("pop") 
+{
+    stack<int> stack_;
+    stack_.push(32);
+    stack_.push(128);
+    stack_.push(256);
+    stack_.pop();
+    REQUIRE(stack_.count() == 2);
+}
