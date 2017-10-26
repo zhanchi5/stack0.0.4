45bd60117d1dd79e9a29525027efe0d49e5b886d
zhanchi5 <zhanchi5@users.noreply.github.com>
2017-10-26 20:39:03 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -1,12 +1,5 @@
 #include <catch.hpp>
 #include <stack0.0.1.hpp>
-TEST_CASE("push") 
-{
-    stack<int> stack_;
-    stack_.push(32);
-    stack_.push(64);
-    REQUIRE(stack_.count() == 2);
-}
 
 TEST_CASE("pop") 
 {
@@ -16,4 +9,24 @@ TEST_CASE("pop")
     stack_.push(256);
     stack_.pop();
     REQUIRE(stack_.count() == 2);
+}
+
+TEST_CASE("push", "") {
+    stack<int> v1;
+    v1.push(51);
+    v1.push(513);
+    v1.push(256);
+    v1.push(123);
+    v1.push(9853);
+    REQUIRE(v1.count() == 5);
+}
+
+TEST_CASE("top", "") {
+    stack<int> v2;
+    v2.push(17);
+    v2.push(413);
+    v2.push(769);
+    v2.push(12);
+    v2.top();
+    REQUIRE(v2.count() == 4);
 }
