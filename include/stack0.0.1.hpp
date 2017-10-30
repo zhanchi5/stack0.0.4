08dcbb44179af17aa16dbc32ebced21535f5cfd0
zhanchi5 <zhanchi5@users.noreply.github.com>
2017-10-30 10:33:15 +0300

    Update stack0.0.1.hpp

--- include/stack0.0.1.hpp
+++ include/stack0.0.1.hpp
@@ -4,83 +4,120 @@
 #include <utility>
 
 template <typename T>
-class stack{
+class stack
+{
+private:
+	void swap(stack<T> &) noexcept;
+
 public:
-    	stack() noexcept;
-        size_t count() const noexcept;
-    	void push(T const &) /* strong */;
-    	void pop()  /*strong*/;
-	T top()  /*strong*/;
-    	
+	stack() noexcept;
+	stack(stack<T> const &) /* strong */;
+	stack<T>& operator = (stack<T> const &)  noexcept;
+	size_t count() const noexcept;
+	void push(T const &) /* strong */;
+	void pop() /* strong */;
+	T top() /* strong */;
+
 
 private:
-	void swap(stack<T>& other) noexcept;
-   	T * array_;
-    	size_t array_size_;
-    	size_t count_;
-	
+	T* array_;
+	size_t array_size_;
+	size_t count_;
 };
 
 template <typename T>
-stack<T>::stack() noexcept : count_(0), array_size_(0), array_(nullptr){}
+stack<T>::stack() noexcept : count_(0), array_size_(0), array_(nullptr)
+{}
 
 template <typename T>
 size_t stack<T>::count() const noexcept
 {
-    return count_;
+	return count_;
 }
 
+template <typename T>
+stack<T>::stack(stack<T> const& copy)
+{
+	T* temp = new T[copy.array_size_];
+	array_size_ = copy.array_size_;
+	count_ = copy.count_;	
+	array_ = temp;
+	try
+	{
+		std::copy(copy.array_, copy.array_ + count_, array_);	
+	}
+	catch( ... )
+	{
+		std::cerr << "Error! Try again!" << std::endl;
+		delete[] array_;
+	}
+}
 
+template <typename T>
+stack<T>& stack<T>::operator=(stack<T> const& other) noexcept
+{
+	if (this != &other)
+	{
+		stack<T> temp(other);
+		swap(temp);
+	}
+	
+	return *this;
+}
 
 template <typename T>
 void stack<T>::swap(stack<T>& other) noexcept
 {
-    std::swap(array_, other.array_);
-    std::swap(array_size_, other.array_size_);
-    std::swap(count_, other.count_);
+	std::swap(array_, other.array_);
+	std::swap(array_size_, other.array_size_);
+	std::swap(count_, other.count_);
 }
 
+template <typename T>
+void stack<T>::push(T const& value)
+{
+	if (array_size_ == count_)
+	{
+		size_t size = array_size_;
+		if (size == 0)
+			size = 1;
+		else
+			size = array_size_ * 2;
+
+		T* temp = new T[size];
+		std::copy(array_, array_ + count_, temp);
+
+		array_size_ = size;
+		delete[] array_;
+		array_ = temp;
+	}
+	
+	array_[count_] = value;
+	++count_;
+}
 
 template <typename T>
-void stack<T>::pop(){
-	if (count_ == 0){
-		std::cout << "Stack is empty! Try again!" << "\n";
+void stack<T>::pop()
+{
+	if (count_ == 0)
+	{
+		throw "Stack is empty! Try again!\n";
 	}
-	else{
+	else
+	{
 		--count_;
 	}
 }
 
 template <typename T>
-T stack<T>::top(){
-	if (count_ == 0){
+T stack<T>::top()
+{
+	if (count_ == 0)
+	{
 		throw "Stack is empty!";
 	}
-	else{
+	else
+	{
 		return array_[count_ - 1];
 	}
 }
-template <typename T>
-void stack<T>::push(T const &val){
-	
-	if (array_size_ == count_){
-		
-		size_t size_ = array_size_;
-		if (size_ == 0){
-			size_ = 1;
-		}
-		else{
-			size_ = array_size_ * 2;
-		}
-
-		T* temp = new T[size_];
-		std::copy(array_, array_ + count_, temp);
-
-		array_size_ = size_;
-		delete[] array_;
-		array_ = temp;
-	}
-	array_[count_] = val;
-++count_;
-}
-
